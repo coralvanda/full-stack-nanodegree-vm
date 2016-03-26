0d98db4d724a2ababc73bb836a461e1de7ef1de0
Eric <coralvanda@gmail.com>
2016-03-26 13:44:53 +0100

    Finish problem set 1 - no idea what I'm doing and no way to test it!

--- vagrant/Full stack class/Problem set 1/puppies.py
+++ vagrant/Full stack class/Problem set 1/puppies.py
@@ -71,3 +71,46 @@ class Adopter(Base):
 
 engine = create_engine('sqlite:///puppyshelter.db')
 Base.metadata.create_all(engine)
+
+'''  Not sure whether this belongs in this file or not
+
+from sqlalchemy.orm import sessionmaker
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
+def check_in(puppy, shelter):
+    if shelter.current_occupancy < shelter.maximum_capacity:
+        shelter.current_occupancy += 1
+        puppy.shelter_id = shelter.id
+        checked_in = True
+        session.add(puppy)
+        session.add(shelter)
+        session.commit()
+    else:
+        shelters = session.query(Shelter).order_by(current_occupancy)
+        checked_in = False
+        for shelter in shelters:
+            if shelter.current_occupancy < shelter.maximum_capacity:
+                shelter.current_occupancy += 1
+                puppy.shelter_id = shelter.id
+                checked_in = True
+                session.add(puppy)
+                session.add(shelter)
+                session.commit()
+        if not checked_in:
+            raise ValueError("All shelters are full, \
+                time to open a new one!")
+
+
+def adopt_puppy(puppy, family):
+    shelter = session.query(Shelter).filter_by(id = puppy.shelter.id).one()
+    shelter.current_occupancy -= 1
+    session.add(shelter)
+    puppy.shelter_id = None
+    for person in family:
+        puppy.adopters.append(person.id)
+    session.add(puppy)
+    session.commit()
+'''
+
