f352f31ea8b7aa6f39eec8af35736fc9c7c63a45
Eric <coralvanda@gmail.com>
2016-03-29 13:02:43 +0200

    Complete edit menu page using flask

--- vagrant/full stack class/project.py
+++ vagrant/full stack class/project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -24,15 +24,37 @@ def restaurantMenu(restaurant_id):
 
 # Task 1: Create route for newMenuItem function here
 
-@app.route('/restaurants/<int:restaurant_id>/new/')
+@app.route('/restaurants/<int:restaurant_id>/new/',
+    methods = ['GET', 'POST'])
 def newMenuItem(restaurant_id):
-    return "page to create a new menu item. Task 1 complete!"
+    if request.method == 'POST':
+        newItem = MenuItem(name = request.form['name'], 
+            restaurant_id = restaurant_id)
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', 
+            restaurant_id = restaurant_id))
+    else:
+        return render_template('newmenuitem.html', 
+            restaurant_id = restaurant_id)
 
 # Task 2: Create route for editMenuItem function here
 
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/')
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/',
+    methods = ['GET', 'POST'])
 def editMenuItem(restaurant_id, menu_id):
-    return "page to edit a menu item. Task 2 complete!"
+    editedItem = session.query(MenuItem).filter_by(id = 
+        menu_id).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            editedItem.name = request.form['name']
+        session.add(editedItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id = 
+            restaurant_id))
+    else:
+        return render_template('editmenuitem.html', restaurant_id =
+            restaurant_id, menu_id = menu_id, i = editedItem)
 
 # Task 3: Create a route for deleteMenuItem function here
 
