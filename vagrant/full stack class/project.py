fcf03ec59665fb79230dc883b06597709023fafd
Eric <coralvanda@gmail.com>
2016-03-29 13:51:15 +0200

    Complete delete menu page and finish styling

--- vagrant/full stack class/project.py
+++ vagrant/full stack class/project.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for
+from flask import request, redirect, flash
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -32,6 +33,7 @@ def newMenuItem(restaurant_id):
             restaurant_id = restaurant_id)
         session.add(newItem)
         session.commit()
+        flash("new menu item created!")
         return redirect(url_for('restaurantMenu', 
             restaurant_id = restaurant_id))
     else:
@@ -50,6 +52,7 @@ def editMenuItem(restaurant_id, menu_id):
             editedItem.name = request.form['name']
         session.add(editedItem)
         session.commit()
+        flash("menu item changed!")
         return redirect(url_for('restaurantMenu', restaurant_id = 
             restaurant_id))
     else:
@@ -58,10 +61,22 @@ def editMenuItem(restaurant_id, menu_id):
 
 # Task 3: Create a route for deleteMenuItem function here
 
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/')
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/',
+    methods = ['GET', 'POST'])
 def deleteMenuItem(restaurant_id, menu_id):
-    return "page to delete a menu item. Task 3 complete!"
+    item_to_delete = session.query(MenuItem).filter_by(id = 
+        menu_id).one()
+    if request.method == 'POST':
+        session.delete(item_to_delete)
+        session.commit()
+        flash("menu item removed!")
+        return redirect(url_for('restaurantMenu', restaurant_id = 
+            restaurant_id))
+    else:
+        return render_template('deletemenuitem.html', 
+            restaurant_id = restaurant_id, i = item_to_delete)
 
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
